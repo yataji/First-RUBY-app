7571e9289dd7e0fb95efc0fde8ef4e745624f46c
Yassine Taji <yataji@e1r7p10.1337.ma>
2020-01-01 21:50:30 +0100

    app

---
+++ app/controllers/yataji_controller.rb
@@ -0,0 +1,4 @@
+class YatajiController < ApplicationController
+  def hello
+  end 
+end
---
+++ test/controllers/yataji_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class YatajiControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
